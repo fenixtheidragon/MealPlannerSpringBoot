5d838bd8f7b485fd7fe277df32679e8998162c0e
fenixtheidragon <fenixtheidragon@gmail.com>
2024-04-12 08:44:47 +0300

    Changed order of methods

--- src/main/java/com/example/mealplanner/services/IngredientService.java
+++ src/main/java/com/example/mealplanner/services/IngredientService.java
@@ -3,16 +3,15 @@ package com.example.mealplanner.services;
 import com.example.mealplanner.models.basic.Ingredient;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IngredientService {
-  Optional<Ingredient> findByName(String name);
-
-  Optional<Ingredient> findById(Long id);
+  Ingredient save(Ingredient ingredient);
 
   List<Ingredient> findAll();
 
-  Ingredient save(Ingredient ingredient);
+  Ingredient findById(Long id);
+
+  Ingredient findByName(String name);
 
   Ingredient update(Ingredient ingredient);
 
