8f8d16d38259f9805f41583364099b8f08e178cc
fenix <fenixtheidragon@gmail.com>
2024-04-14 22:16:05 +0300

    changed long to Long in id

--- src/main/java/com/example/mealplanner/models/basic/Ingredient.java
+++ src/main/java/com/example/mealplanner/models/basic/Ingredient.java
@@ -18,7 +18,7 @@ import java.util.Set;
 public class Ingredient {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private long id;
+  private Long id;
 
   @Column(unique = true)
   @NonNull
