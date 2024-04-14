d57142b57e7b49b0eac582797f930deec2d5375b
fenix <fenixtheidragon@gmail.com>
2024-04-14 03:16:50 +0300

    implemented functionality for all methods;

--- src/main/java/com/example/mealplanner/impl/IngredientServiceImpl.java
+++ src/main/java/com/example/mealplanner/impl/IngredientServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.mealplanner.impl;
 
+import com.example.mealplanner.helpers.exceptions.ResourceAlreadyExistsException;
+import com.example.mealplanner.helpers.exceptions.ResourceNotFoundException;
 import com.example.mealplanner.models.basic.Ingredient;
 import com.example.mealplanner.repositories.IngredientRepository;
 import com.example.mealplanner.services.IngredientService;
@@ -10,71 +12,65 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 @Primary
 public class IngredientServiceImpl implements IngredientService {
+  private final static String resourceClassName = Ingredient.class.getName();
   private final IngredientRepository repository;
 
   @Override
   public ResponseEntity<Ingredient> findByName(String name) {
-     return repository.findByName(name)
-         .orElseThrow(Res);
-     return createResponseEntityFrom(optionalIngredient);
+    var ingredient = repository.findByName(name)
+        .orElseThrow(() -> new ResourceNotFoundException(resourceClassName, "name", name));
+    return new ResponseEntity<>(ingredient, HttpStatus.FOUND);
   }
 
   @Override
   public ResponseEntity<Ingredient> findById(Long id) {
-    var optionalIngredient = repository.findById(id);
-    return createResponseEntityFrom(optionalIngredient);
+    var ingredient = repository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException(resourceClassName, "id", String.valueOf(id)));
+    return new ResponseEntity<>(ingredient, HttpStatus.FOUND);
   }
 
   @Override
   public ResponseEntity<List<Ingredient>> findAll() {
-    var optionalIngredientsList = repository.findAll();
-    if (!optionalIngredientsList.isEmpty()) {
-      return new ResponseEntity<>(repository.findAll(), HttpStatus.FOUND);
-    }
-    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    var ingredientList = repository.findAll();
+    return new ResponseEntity<>(ingredientList, HttpStatus.OK);
   }
 
   @Override
   public ResponseEntity<Ingredient> save(Ingredient ingredient) {
     var optionalIngredient = repository.findByName(ingredient.getName());
-    if (optionalIngredient.isEmpty()) {
-      return new ResponseEntity<>(repository.save(ingredient), HttpStatus.CREATED);
-    }
-    return new ResponseEntity<>(HttpStatus.CONFLICT);
-  }
-
-  @Override
-  public ResponseEntity<Ingredient> update(Ingredient ingredient) {
-    var ingredientToBeUpdated = repository.findById(ingredient.getId());
-    if (ingredientToBeUpdated.isPresent()) {
-
-    }
-    return repository.save(ingredient);
-  }
-
-  @Override
-  public ResponseEntity<HttpStatus> deleteByName(String name) {
-    var ingredientToBeDeleted = findAll().stream()
-        .filter(ingredient -> ingredient.getName().equals(name))
-        .findFirst()
-        .orElseThrow(NoSuchElementException::new);
-    if (ingredientToBeDeleted != null) {
-      repository.delete(ingredientToBeDeleted);
-    }
-    return null;
-  }
-
-  private ResponseEntity<Ingredient> createResponseEntityFrom(Optional<Ingredient> optionalIngredient) {
     if (optionalIngredient.isPresent()) {
-      return new ResponseEntity<>(optionalIngredient.get(), HttpStatus.FOUND);
+      throw new ResourceAlreadyExistsException(resourceClassName, "name", ingredient.getName());
     }
-    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    return new ResponseEntity<>(repository.save(ingredient), HttpStatus.CREATED);
+  }
+
+  @Override
+  public ResponseEntity<Ingredient> update(Ingredient newIngredient) {
+    validateUpdateRequest(newIngredient);
+    return new ResponseEntity<>(repository.save(newIngredient), HttpStatus.OK);
+  }
+
+  @Override
+  public ResponseEntity<HttpStatus> deleteById(Long id) {
+    repository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException(resourceClassName, "id", String.valueOf(id)));
+    repository.deleteById(id);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  private void validateUpdateRequest(Ingredient newIngredient) {
+    repository.findById(newIngredient.getId())
+        .orElseThrow(() ->
+            new ResourceNotFoundException(resourceClassName, "id", String.valueOf(newIngredient.getId()))
+        );
+    var optionalIngredient = repository.findByName(newIngredient.getName());
+    if (optionalIngredient.isPresent()) {
+      throw new ResourceAlreadyExistsException(resourceClassName, "name", newIngredient.getName());
+    }
   }
 }
