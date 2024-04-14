2e32f9f280842d39182a5d94ae8abb8a03766d01
fenix <fenixtheidragon@gmail.com>
2024-04-14 03:17:29 +0300

    changed because of ResourceException;

--- src/main/java/com/example/mealplanner/helpers/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/mealplanner/helpers/exceptions/ResourceNotFoundException.java
@@ -1,8 +1,8 @@
 package com.example.mealplanner.helpers.exceptions;
 
-public class ResourceNotFoundException extends RuntimeException {
-private final static String mainMessageText = "%s with %s %s not found.";
-    public ResourceNotFoundException(String fieldClassName, String fieldName, String fieldValueAsString) {
-    super(String.format(mainMessageText, fieldClassName, fieldName, fieldValueAsString));
+public class ResourceNotFoundException extends ResourceException {
+
+  public ResourceNotFoundException(String fieldClassName, String fieldName, String fieldValueAsString) {
+    super(fieldClassName, fieldName, fieldValueAsString, "not found");
   }
 }
