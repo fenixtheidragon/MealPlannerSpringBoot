f28a19cae4cd3a9d41b37b41b36d0d8a6d9f2d25
fenixtheidragon <fenixtheidragon@gmail.com>
2024-05-02 17:11:00 +0300

    implemented search by letters for ingredients;

--- src/main/java/com/example/mealplanner/services/IngredientService.java
+++ src/main/java/com/example/mealplanner/services/IngredientService.java
@@ -9,5 +9,4 @@ public interface IngredientService extends GeneralService<IngredientDto>{
   ResponseEntity<IngredientDto> findByName(String name);
 
   ResponseEntity<List<IngredientDto>> findByLetters(String letters);
-
 }
